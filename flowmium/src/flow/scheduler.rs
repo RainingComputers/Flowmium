75cc46152fc79b517cb5d26fc68d4577d657c5e3
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-12-31 19:05:27 +0530

    Minimal MVP in memory scheduler code

--- flowmium/src/flow/scheduler.rs
+++ flowmium/src/flow/scheduler.rs
@@ -1,13 +1,9 @@
-use std::{
-    collections::{binary_heap::Iter, BTreeMap},
-    iter::Filter,
-};
-
-use super::model::{Flow, Task};
+use super::model::Task;
 
 struct FlowState {
     id: usize,
     plan: Vec<Vec<usize>>,
+    current_stage: usize,
     running_tasks: Vec<usize>,
     finished_tasks: Vec<usize>,
     task_definitions: Vec<Task>,
@@ -24,6 +20,7 @@ impl FlowState {
         FlowState {
             id,
             plan,
+            current_stage: 0,
             running_tasks: vec![],
             finished_tasks: vec![],
             task_definitions,
@@ -32,30 +29,90 @@ impl FlowState {
     }
 }
 
-struct ExecutorState {
+enum SchedulerError {
+    FlowDoesNotExist,
+    StageDoesNotExist,
+    TaskDoesNotExist,
+}
+
+struct Scheduler {
     flow_runs: Vec<FlowState>,
 }
 
-impl ExecutorState {
+impl Scheduler {
     fn create_flow(
-        self,
+        mut self,
         flow_name: String,
-        plan: Vec<Vec<i32>>,
+        plan: Vec<Vec<usize>>,
         task_definitions: Vec<Task>,
     ) -> usize {
-        // TODO: generate random unique id and insert into map
+        let id = self.flow_runs.len();
+
+        self.flow_runs.push(FlowState::create_flow_state(
+            id,
+            flow_name,
+            plan,
+            task_definitions,
+        ));
 
         return 0;
     }
 
-    fn mark_task_finished(self, flow_id: usize, task_id: usize) {
-        // TODO: Move the task from running to finished
+    fn mark_task_running(mut self, flow_id: usize, task_id: usize) -> Result<(), SchedulerError> {
+        let Some(flow) = self.flow_runs.get_mut(flow_id) else {
+            return Err(SchedulerError::FlowDoesNotExist);
+        };
+
+        flow.running_tasks.push(task_id);
+
+        return Ok(());
     }
 
-    fn schedule_next_task(self, flow_id: usize) -> Option<Task> {
-        // TODO: Fetch flow, look at plan, running tasks and finished tasks, return next task to run if any
+    fn schedule_next_stage<'a>(
+        &'a self,
+        flow_id: usize,
+    ) -> Result<Option<Vec<&Task>>, SchedulerError> {
+        let Some(flow) = self.flow_runs.get(flow_id) else {
+            return Err(SchedulerError::FlowDoesNotExist);
+        };
 
-        None
+        let Some(stage) = flow.plan.get(flow.current_stage) else {
+            return Err(SchedulerError::StageDoesNotExist);
+        };
+
+        for task_id in stage {
+            match flow.finished_tasks.get(*task_id) {
+                None => {
+                    return Ok(None);
+                }
+                _ => (),
+            }
+        }
+
+        let mut stage_tasks: Vec<&Task> = vec![];
+
+        for option_task in stage.into_iter().map(|id| flow.task_definitions.get(*id)) {
+            match option_task {
+                None => {
+                    return Err(SchedulerError::TaskDoesNotExist);
+                }
+                Some(task) => stage_tasks.push(task),
+            }
+        }
+
+        return Ok(Some(stage_tasks));
+    }
+
+    fn mark_task_finished(mut self, flow_id: usize, task_id: usize) -> Result<(), SchedulerError> {
+        let Some(flow) = self.flow_runs.get_mut(flow_id) else {
+            return Err(SchedulerError::FlowDoesNotExist);
+        };
+
+        flow.running_tasks.retain(|id| *id != task_id);
+
+        flow.finished_tasks.push(task_id);
+
+        return Ok(());
     }
 
     fn get_running_flows(self) -> Vec<usize> {
