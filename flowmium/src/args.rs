d9b1ef1540a7eb84e420aacc06a415c3c890e72e
RainingComputers <vishnu.vish.shankar@gmail.com>
2023-02-23 23:34:30 +0530

    Add command line arguments

---
+++ flowmium/src/args.rs
@@ -0,0 +1,34 @@
+use gumdrop::Options;
+
+#[derive(Debug, Options)]
+pub struct FlowmiumOptions {
+    #[options(help = "Print help message")]
+    pub help: bool,
+
+    #[options(command, required)]
+    pub command: Option<Command>,
+}
+
+#[derive(Debug, Options)]
+pub enum Command {
+    #[options(help = "Run flowmium sidecar container")]
+    Sidecar(SidecarOpts),
+    #[options(help = "Run flowmium main container")]
+    Main(MainOpts),
+}
+
+#[derive(Debug, Options)]
+pub struct SidecarOpts {
+    #[options(help = "Launch init sidecar which will download all required inputs")]
+    pub init: bool,
+    #[options(
+        help = "Launch main sidecar which will wait for main container and upload all outputs"
+    )]
+    pub wait: bool,
+}
+
+#[derive(Debug, Options)]
+pub struct MainOpts {
+    #[options(help = "Command for main container")]
+    pub cmd: Vec<String>,
+}
